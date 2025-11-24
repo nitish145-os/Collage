c2030834929f2ab5a897b4ce9b3e3070fa6b8244
nitish145-os <77478808+nitish145-os@users.noreply.github.com>
2025-11-24 10:16:01 +0530

    a1

---
+++ arrays/transposeMatrix.c
@@ -0,0 +1,24 @@
+//Nitish Thombre 25070521099
+
+#include <stdio.h>
+int main() {
+  int n;
+  scanf("%d", &n);
+  int matrix[n][n];
+  int i, j;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      printf("%d ", matrix[j][i]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
---
+++ operators/relational.c
@@ -0,0 +1,37 @@
+//Nitish Thombre 25070521099
+
+#include <stdio.h>
+
+int main(){
+	int inp1 , inp2;
+
+	printf("Enter two numbers: ");
+	scanf("%d %d" , &inp1,&inp2);
+
+	printf("%d < %d: " , inp1 , inp2);
+	printf("%s" ,inp1 < inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d <= %d: ", inp1 , inp2);
+	printf("%s" ,inp1 <= inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d > %d: ", inp1 , inp2);
+	printf("%s" ,inp1 > inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d >= %d: ", inp1 , inp2);
+	printf("%s" ,inp1 >= inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d == %d: ", inp1 , inp2);
+	printf("%s" ,inp1 == inp2 ? "True" : "False");
+	printf("\n");
+
+	printf("%d != %d: ", inp1 , inp2);
+	printf("%s" ,inp1 != inp2 ? "True" : "False");
+	printf("\n");
+
+	return 0;
+
+}
